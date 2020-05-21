87140253fcb5db21837e41c1425a842cb7f7060d
psypher <ryan.storm@gmail.com>
2020-05-21 06:48:47 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# typescript-fgsrus
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-fgsrus)
---
+++ index.ts
@@ -0,0 +1,8 @@
+// Import stylesheets
+import './style.css';
+
+const canvas = document.getElementById("canvas");
+
+// Write TypeScript code!
+const appDiv: HTMLElement = document.getElementById('app');
+appDiv.innerHTML = `<h1></h1>`;
